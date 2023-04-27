8b458996524a50a100d9b888c251ffc33828c647
KevinBett <kevinbett458@gmail.com>
2023-04-27 10:28:25 +0300

    added bonus feature

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
 function Article(props) {
-  const { title, date = "January 1, 1970", preview } = props;
+  const { title, date = "January 1, 1970", preview, minutes } = props;
+
+  // Calculate the number of coffee cups or bento boxes based on the estimated time to read
+  let cupsOrBoxes = null;
+  if (minutes < 30) {
+    cupsOrBoxes = '☕️'.repeat(Math.ceil(minutes / 5));
+  } else {
+    cupsOrBoxes = '🍱'.repeat(Math.ceil(minutes / 10));
+  }
+
   return (
     <article>
       <h3>{title}</h3>
-      <small>{date}</small>
+      <small>{date} - {cupsOrBoxes} {minutes} min read</small>
       <p>{preview}</p>
     </article>
   )
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,13 +4,21 @@ import Article from './Article';
 const ArticleList = ({ posts }) => {
   return (
     <main>
-      {posts.map((post) => (
-        <Article 
-        key={post.id} 
-        title={post.title}
-        date={post.date}
-        preview={post.preview} />
-      ))}
+      {posts.map((post) => {
+        // Calculate the estimated time to read based on the length of the article
+        const wordsPerMinute = 200;
+        const lengthInWords = post.preview.split(' ').length;
+        const minutes = Math.ceil(lengthInWords / wordsPerMinute);
+
+        return (
+          <Article 
+            key={post.id} 
+            title={post.title}
+            date={post.date}
+            preview={post.preview}
+            minutes={minutes} />
+        )
+      })}
     </main>
   );
 };
